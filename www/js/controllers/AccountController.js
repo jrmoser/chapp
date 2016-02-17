1519ffaeae34440d6d06fe3b2ae842e3e3ddc715
Zach Huffman <zether8@gmail.com>
2016-02-17 09:58:14 -0700

    starting on the Firebase user registration

--- www/js/controllers/AccountController.js
+++ www/js/controllers/AccountController.js
@@ -33,7 +33,19 @@
       else {
         $scope.reg = false;
       }
-    }
+    };
+
+    var ref = new Firebase("https://firechatmlatc.firebaseio.com");
+    ref.createUser({
+      email    : "bobtony@firebase.com",
+      password : "correcthorsebatterystaple"
+    }, function(error, userData) {
+      if (error) {
+        console.log("Error creating user:", error);
+      } else {
+        console.log("Successfully created user account with uid:", userData.uid);
+      }
+    });
 
   }
 
