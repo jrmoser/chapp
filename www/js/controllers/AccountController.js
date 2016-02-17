9fca260637e948125c503a060ed54bbea411dc8c
Zach Huffman <zether8@gmail.com>
2016-02-17 09:53:05 -0700

    Finished the UI for login and registration
    Added the ability to switch between Login UI and Register UI
    
    TODO:
    make the submit buttons do something

--- www/js/controllers/AccountController.js
+++ www/js/controllers/AccountController.js
@@ -19,10 +19,21 @@
       console.log('The user was registered! Not really. This function does nothing. :(');
       console.log(firstname);
       console.log(lastname);
-      console.log(email)
+      console.log(email);
       console.log(username);
       console.log(password);
     };
+
+    $scope.reg = false;
+
+    $scope.togglereg = function(){
+      if (!$scope.reg) {
+        $scope.reg = true;
+      }
+      else {
+        $scope.reg = false;
+      }
+    }
 
   }
 
