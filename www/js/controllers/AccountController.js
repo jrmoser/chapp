e58ba300409c3d8bddb5387795da63c265f00e6b
Zach Huffman <zether8@gmail.com>
2016-02-17 09:09:26 -0700

    Changes to Account page and controller
    Added Login and New User sections
    TODO:
    Hide New User section until “Click here to create an account” is
    clicked.
    Change the Login and Register function so they do something

--- www/js/controllers/AccountController.js
+++ www/js/controllers/AccountController.js
@@ -9,8 +9,19 @@
 
   function AccountController($scope) {
 
-    $scope.settings = {
-      enableFriends: true
+    $scope.login = function login(username, password){
+      console.log('The user was logged in! Not really. This function does nothing. :(')
+      console.log(username);
+      console.log(password);
+    };
+
+    $scope.register = function register(firstname, lastname, email, username, password){
+      console.log('The user was registered! Not really. This function does nothing. :(');
+      console.log(firstname);
+      console.log(lastname);
+      console.log(email)
+      console.log(username);
+      console.log(password);
     };
 
   }
